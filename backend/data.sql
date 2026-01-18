67bca0f4fedcffe013b362323565f2d07c1ef1b1
gi003 <diraheta@ucsc.edu>
2026-01-18 04:09:23 -0800

    I am done
    DONEE

--- backend/data.sql
+++ backend/data.sql
@@ -6,19 +6,19 @@ INSERT INTO users (username, password, metadata) VALUES
     'name', 'Molly Member'
   )
 ),
-('anna', crypt('annaadmin', gen_salt('bf')), jsonb_build_object(
+('CruzHacks', crypt('annaadmin', gen_salt('bf')), jsonb_build_object(
     'email', 'anna@books.com',
     'name', 'Anna Admin'
+  )
+),
+('UCSCTheater', crypt('annaadmin', gen_salt('bf')), jsonb_build_object(
+    'email', 'ucsc@books.com',
+    'name', 'UCSC'
   )
 );
 
 -- Insert sample posts
-INSERT INTO posts (title, content, event_date, coordinates) VALUES
-('Clothing Swap', 'Bring your nicest clothes!', '2025-01-20 10:30:00', '{"x": 524, "y": 1447}'),
-('Hackathon Presentations', 'Cruzly is so good', '2025-01-18 09:00:00', '{"x": 2296, "y": 1083}'),
-('Spongebob Musical', 'Buy tickets https://events.ucsc.edu/event/spongebob-musical/2025-11-16/', '2025-11-16 19:00:00', '{"x": 948, "y": 1443}');
-
--- -- Insert sample groups
--- INSERT INTO groups (name, description) VALUES
--- ('Book Club', 'Discuss favorite books'),
--- ('Literature', 'All things literature');
+INSERT INTO posts (user_id, title, content, event_date, coordinates) VALUES
+((SELECT id FROM users WHERE username = 'molly'),'Clothing Swap', 'Bring your nicest clothes!', '2025-01-20 10:30:00', '{"x": 524, "y": 1447}'),
+((SELECT id FROM users WHERE username = 'CruzHacks'),'Hackathon Presentations', 'Cruzly is so good', '2025-01-18 09:00:00', '{"x": 2296, "y": 1083}'),
+((SELECT id FROM users WHERE username = 'UCSCTheater'),'Spongebob Musical', 'Buy tickets https://events.ucsc.edu/event/spongebob-musical/2025-11-16/', '2025-11-16 19:00:00', '{"x": 948, "y": 1443}');
